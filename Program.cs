ea8b24d2506a60996bb9fcf6e9782b046afa8229
Mark Shternberg <nikita@medvedev-it.ru>
2024-09-28 13:01:57 +0300

    Exception logging to file

--- Program.cs
+++ Program.cs
@@ -1,6 +1,23 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Create folder if doesn/t exist
+var logDirectory = Path.Combine(Directory.GetCurrentDirectory(), "logs");
+if (!Directory.Exists(logDirectory))
+{
+    Directory.CreateDirectory(logDirectory);
+}
+
+// LOGGING
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.File(Path.Combine(logDirectory, "error_log_.log"), rollingInterval: RollingInterval.Minute)
+    .CreateLogger();
+
+builder.Host.UseSerilog();
+
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
@@ -8,14 +25,28 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler(errorApp =>
+    {
+        errorApp.Run(async context =>
+        {
+            var exceptionHandlerFeature = context.Features.Get<IExceptionHandlerFeature>();
+            if (exceptionHandlerFeature?.Error != null)
+            {
+                Log.Error(exceptionHandlerFeature.Error, "An unhandled exception has occurred.");
+            }
+
+            Log.Information("Redirecting to the error page.");
+            context.Response.Redirect("/Error");
+        });
+    });
 }
+else
+{
+    app.UseDeveloperExceptionPage();
+}
+
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapRazorPages();
-
 app.Run();
